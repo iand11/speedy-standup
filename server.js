5c81f529204f8c4a520c61de67546a19d63b83f2
Ian Driscoll <ian.driscoll.o@gmail.com>
2021-12-10 18:00:54 -0700

    Update port

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ MongoClient.connect(uri, { useUnifiedTopology: true })
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 80, function () {
   console.log('listening on 3000')
 })
