ace8989023826222ce49b3e752a07afd431e47f0
Ian Driscoll <ian.driscoll.o@gmail.com>
2021-12-13 16:44:47 -0700

    Add user model, routes, and auth middleware

---
+++ app/routes/user.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+const auth = require('../middleware/auth');
+
+const User = require("../models/user-model");
+
+const { create, login, me } = require('../controllers/user-controllers');
+
+/**
+ * @method - POST
+ * @param - /signup
+ * @description - User SignUp
+ */
+
+router.post(
+  "/signup",
+  [
+    check("name", "Please Enter a Valid name").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Please enter a valid password").isLength({
+      min: 6
+    })
+  ],
+  create
+);
+
+/**
+ * @method - POST
+ * @param - /login
+ * @description - User Login
+ */
+
+router.post(
+  "/login",
+  [
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Please enter a valid password").isLength({
+      min: 6
+    })
+  ],
+  login
+);
+
+/**
+ * @method - GET
+ * @param - /me
+ * @description - User Info
+ */
+
+router.get("/me", auth, me);
+
+module.exports = router;
