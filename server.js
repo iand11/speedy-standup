980f86fe6377a7fbe674f80fd26b8ec8ed3823fc
Ian Driscoll <ian.driscoll.o@gmail.com>
2021-12-14 08:49:00 -0700

    Initial test of web socket

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
 const bodyParser = require('body-parser')
-const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
-const cors = require('cors')
+const cors = require('cors');
+// const { Server } = require("socket.io");
+// const io = new Server(server);
+const ws = require('ws');
 
 const user = require("./app/routes/user");
 const blocker = require('./app/routes/blocker.js');
+
 
 app.use(cors());
 mongoose.Promise = global.Promise;
@@ -16,6 +21,8 @@ const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 
 const uri = `mongodb+srv://${username}:${password}@cluster0.hkrzv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const wsServer = new ws.Server({ noServer: true });
 
 mongoose.connect(uri, {
   useNewUrlParser: true
@@ -26,14 +33,22 @@ mongoose.connect(uri, {
   process.exit();
 });
 
+wsServer.on('connection', socket => {
+  socket.on('message', message => console.log(message));
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+// });
 
 app.use("/user", user);
 
 app.use("/blocker", blocker);
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(4000, function () {
   console.log('listening on 3000')
 })
